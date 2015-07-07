c0f6344e824f1b042fd677696296f434317edd02
Max Wielsch <max.wielsch@googlemail.com>
2015-07-07 13:29:02 +0200

    Change test so it is showing problems with current concurrency implementation better

--- src/test/java/de/muximweb/javafx/testing_asynchonous_logic/ClassToTestTest.java
+++ src/test/java/de/muximweb/javafx/testing_asynchonous_logic/ClassToTestTest.java
@@ -8,11 +8,15 @@ import javafx.beans.property.StringProperty;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 import static org.junit.Assert.assertEquals;
 
 
 /**
- * A test for {@link ClassToTest}.
+ * A test for {@link ClassToTest} using the solution pattern given by
+ * http://blog.buildpath.de/how-to-test-javafx-services/.
  */
 @RunWith(JfxRunner.class)
 public class ClassToTestTest {
@@ -20,13 +24,48 @@ public class ClassToTestTest {
     private ClassToTest cut = new ClassToTest();
 
     @Test
-    public void testLongLastingOperation() {
+    public void testLongLastingOperationCallingOnce() throws ExecutionException, InterruptedException {
         StringProperty resultProperty = new SimpleStringProperty();
-        IntegerProperty progressProperty = new SimpleIntegerProperty(1);
+        IntegerProperty progressProperty = new SimpleIntegerProperty(0);
+
+        CompletableFuture longLastingOperationFuture = newFuture(progressProperty);
 
         cut.execLongLastingOperation(resultProperty, progressProperty);
+        longLastingOperationFuture.get();
 
         assertEquals("An expensive result", resultProperty.get());
         assertEquals(1, progressProperty.get());
+    }
+
+    @Test
+    public void testLongLastingOperationCallingTwice() throws ExecutionException, InterruptedException {
+        StringProperty resultProperty = new SimpleStringProperty();
+        IntegerProperty progressProperty = new SimpleIntegerProperty(0);
+
+        // first call succeeds
+        CompletableFuture longLastingOperationFuture1 = newFuture(progressProperty);
+        cut.execLongLastingOperation(resultProperty, progressProperty);
+        longLastingOperationFuture1.get();
+
+        assertEquals("An expensive result", resultProperty.get());
+        assertEquals(1, progressProperty.get());
+
+        // second call fails as because of {@link Service#checkThread}
+        CompletableFuture longLastingOperationFuture2 = newFuture(progressProperty);
+        cut.execLongLastingOperation(resultProperty, progressProperty);
+        longLastingOperationFuture2.get();
+
+        assertEquals("An expensive result", resultProperty.get());
+        assertEquals(1, progressProperty.get());
+    }
+
+    private CompletableFuture newFuture(IntegerProperty progressProperty) {
+        CompletableFuture completableFuture = new CompletableFuture();
+        progressProperty.addListener((b, o, n) -> {
+            if (n.intValue() == 1) {
+                completableFuture.complete(null);
+            }
+        });
+        return completableFuture;
     }
 }
